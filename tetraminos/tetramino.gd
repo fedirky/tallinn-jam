7d15706492df5c896602e5245f2b305bb9b15e59
fedirky <fedirky@gmail.com>
2024-10-13 00:52:24 +0300

    main nenu + new assets

--- tetraminos/tetramino.gd
+++ tetraminos/tetramino.gd
@@ -2,10 +2,10 @@ extends RigidBody2D
 
 
 func _on_area_2d_body_entered(body) -> void:
-    print("Entered body!, self: ", body == self)
-    if body != self:
-        linear_velocity = Vector2(1.0, 1.0)
-        gravity_scale = 1.0
-        if is_in_group("tetramino_dynamic"):
-            remove_from_group("tetramino_dynamic")
-        add_to_group("tetramino_laying")
+	print("Entered body!, self: ", body == self)
+	if body != self:
+		linear_velocity = Vector2(1.0, 1.0)
+		gravity_scale = 1.0
+		if is_in_group("tetramino_dynamic"):
+			remove_from_group("tetramino_dynamic")
+		add_to_group("tetramino_laying")
